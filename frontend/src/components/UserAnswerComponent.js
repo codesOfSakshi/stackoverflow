5e3fcbd7b189b80deb4268b38375a69c73167cb2
Sakshi <sakshi.mishra@sjsu.edu>
2022-04-27 14:11:03 -0700

    user ans component

---
+++ frontend/src/components/UserAnswerComponent.js
@@ -0,0 +1,72 @@
+
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+
+
+export default function UserAnswerComponent() {
+
+    return (
+
+            <div id="question-summary-61933462" className="s-post-summary s-post-summary__minimal js-post-summary"
+                 data-post-id="61933462" data-post-type-id="1">
+                <div className="s-post-summary--stats js-post-summary-stats">
+
+
+                    <div className="s-post-summary--stats-item s-post-summary--stats-item__emphasized"
+                         title="Score of 1">
+                        <span className="s-post-summary--stats-item-number">1</span>
+                        <span className="s-post-summary--stats-item-unit">vote</span>
+                    </div>
+                    <div className="s-post-summary--stats-item has-answers " title="1 answer">
+                        <span className="s-post-summary--stats-item-number">1</span>
+                        <span className="s-post-summary--stats-item-unit">answer</span>
+                    </div>
+                    <div className="s-post-summary--stats-item " title="297 views">
+                        <span className="s-post-summary--stats-item-number">297</span>
+                        <span className="s-post-summary--stats-item-unit">views</span>
+                    </div>
+
+
+                </div>
+                <div className="s-post-summary--content">
+
+                    <h3 className="s-post-summary--content-title">
+
+
+                        <a href="/questions/61933462/aerospike-as-cache" className="s-link">Aerospike as cache</a>
+                    </h3>
+                    <div className="s-post-summary--meta">
+                        <div className="s-post-summary--meta-tags tags js-tags t-spring t-caching t-aerospike">
+
+                            <a href="/questions/tagged/spring" className="post-tag flex--item mt0 js-tagname-spring"
+                               title="" rel="tag">spring</a> <a href="/questions/tagged/caching"
+                                                                className="post-tag flex--item mt0 js-tagname-caching"
+                                                                title="" rel="tag">caching</a> <a
+                            href="/questions/tagged/aerospike" className="post-tag flex--item mt0 js-tagname-aerospike"
+                            title="" rel="tag">aerospike</a>
+                        </div>
+
+
+                        <div className="s-user-card s-user-card__minimal">
+
+
+                            <div className="s-user-card--info">
+                                <div className="s-user-card--link d-flex gs4">
+
+                                </div>
+
+
+                            </div>
+
+                            <time className="s-user-card--time">asked <span title="2020-05-21 11:26:53Z"
+                                                                            className="relativetime">May 21, 2020 at 11:26</span>
+                            </time>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+    );
+}
