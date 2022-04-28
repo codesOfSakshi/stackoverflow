a9060e6340a583c7b295a2398cb9b5ef94ebb773
Sakshi <sakshi.mishra@sjsu.edu>
2022-04-28 11:39:47 -0700

    edit user detail page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import User from './pages/user.js';
+import UserDetail from './pages/userDetail.js';
 
 
 function App() {
   return (
     <Routes>
         <Route exact path="/user/:userId" element={<User/>} />
+        <Route exact path="/user/edit/:userId" element={<UserDetail/>} />
     </Routes>
   );
 }
--- frontend/src/pages/userDetail.js
+++ frontend/src/pages/userDetail.js
@@ -1,186 +1,32 @@
-import React, { useState,  useEffect } from "react";
-import Cookies from 'universal-cookie';
+import React, { useState } from "react";
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import ImageUploadCard from '../components/user/ImageUpload';
-import CardMedia from "@mui/material/CardMedia";
 
 
-import {
-    Grid,
-    Paper,
-    Typography,
-    IconButton,
-    createMuiTheme,
-    MuiThemeProvider,
-    makeStyles,
-    TextField,
-    Button
-} from "@material-ui/core";
-import EditIcon from "@material-ui/icons/Edit";
-const cookies = new Cookies();
-let theme = createMuiTheme();
-theme.typography.h6 = {
-    fontSize: "1rem",
-    "@media (min-width:900px)": {
-        fontSize: "1.05rem"
-    },
-    "@media (min-width:1000px)": {
-        fontSize: "1.1rem"
-    },
-    "@media (min-width:1200px)": {
-        fontSize: "1.2rem"
-    },
-    "@media (min-width:1300px)": {
-        fontSize: "1.25rem"
-    }
-};
-
-// dummy data
-const user = {
-    name: "name",
-    location: "location",
-    title: "title"
-    //   imagelink: "This is my image",
-};
-
-const mapInformation = {
-    name: "Name",
-    location: "location",
-    title: "title"
-};
-
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        backgroundColor: "orange",
-        color: "white",
-        padding: "1em",
-        width: "60%",
-        [theme.breakpoints.down(1200)]: {
-            width: "70%"
-        },
-        [theme.breakpoints.down(1000)]: {
-            width: "80%"
-        },
-        [theme.breakpoints.down(900)]: {
-            width: "90%"
-        },
-        [theme.breakpoints.down(800)]: {
-            width: "100%"
-        }
-    },
-    form: {
-        backgroundColor: "white",
-        color: "orange ",
-        padding: "1em",
-        width: "60%",
-        [theme.breakpoints.down(1200)]: {
-            width: "70%"
-        },
-        [theme.breakpoints.down(1000)]: {
-            width: "80%"
-        },
-        [theme.breakpoints.down(900)]: {
-            width: "90%"
-        },
-        [theme.breakpoints.down(800)]: {
-            width: "100%"
-        }
-    }
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
 }));
 
-const UserInfoFormItem = (formState, onChange, propt, index) => {
+export default function ReviewCard({seller}) {
 
+    ;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const classes = useStyles();
-    return (
-        <Grid
-            item
-            xs={6}
-            key={`display-${index}`}
-            container
-            direction="column"
-            alignItems="center"
-        >
-            <Paper className={classes.form}>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">{mapInformation[propt]}</Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <TextField
-                        defaultValue={formState[propt]}
-                        name={Object.keys(user)[index]}
-                        onChange={onChange}
-                    />
-                </Grid>
-            </Paper>
-        </Grid>
-    );
-};
-
-const UserInfoGridItem = (formState, propt, index) => {
-    const classes = useStyles();
-    return (
-        <Grid
-            item
-            xs={6}
-            key={`display-${index}`}
-            container
-            direction="column"
-            alignItems="center"
-        >
-            <Paper className={classes.paper}>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">{mapInformation[propt]}</Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h6">{formState[propt]}</Typography>
-                </Grid>
-            </Paper>
-        </Grid>
-    );
-};
-
-export default function Profile() {
-    const [isForm, setIsForm] = useState(false);
-    const [formInput, setFormInput] = useState([]);
-    const [url, setUrl] = useState(cookies.get('imageUrl'));
-
+    const [expanded, setExpanded] = React.useState(false);
     const [open, setOpen] = React.useState(false);
-    let userId =  cookies.get('userId')
-    // ADD AJAX CALLS HERE IN A USE EFFECT HOOK, use api call to update formInput as the user info, once it works replace the initial state of form info with a blank string
-
-
-
-
-    const handleEdit = () => setIsForm(true);
-
+    const [url, setUrl] = useState("");
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -190,94 +36,81 @@ export default function Profile() {
     };
 
     const handleSave = () => {
-        setUrl(cookies.get('imageUrl'))
-        console.log(cookies.get('imageUrl'))
+        setUrl("")
+
+        const req1= {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                shop_image_url: "",
+                shop_id:seller.shop_id
+            })
+        }
+        fetch("http://localhost:5000/shop/updateImage",req1)
+            .then(response => {
+                if(response.status===200)
+                {   console.log("Image is updated successfully")
+                    console.log(response.body)
+
+                }
+                else{
+                    console.log("Image update Failed")
+                }
+
+            })
     };
 
-    const handleChange = (e) => {
-        const value = e.target.value;
-        setFormInput((prev) => ({
-            ...prev,
-            [e.target.name]: value
-        }));
-    };
-
-    const handleSubmit = () => {
-        setFormInput(formInput);
-
-        console.log("Product")
-
-
-        const toggleRender = () => {
-            if (isForm) {
-                return Object.keys(user).map((key, index) =>
-                    UserInfoFormItem(formInput, handleChange, key, index)
-                );
-            }
-
-            return Object.keys(user).map((key, index) =>
-                UserInfoGridItem(formInput, key, index)
-            );
-        };
-    }
-
-    const handlePopUp = () => {
-
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
     };
 
     return (
-        <div>
-
-            <div>
-
+        <Card sx={{ maxWidth: 800, ml:50 }}>
+            <Card sx={{ maxWidth: 200, ml:50 }}>
                 <Dialog open={open} onClose={handleClose}>
-                    <DialogTitle>Subscribe</DialogTitle>
+
                     <DialogContent>
-
                         <ImageUploadCard/>
-
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={handleSave}>Save</Button>
                         <Button onClick={handleClose}>Close</Button>
-
                     </DialogActions>
                 </Dialog>
+            </Card>
 
 
+            <h1>Public information</h1>
+            <div className="bg-white d:bg-black-025 bar-md ba bc-black-075 mb48 p24 sm:p12">
+                <div >
+                    <div className="gravatar-wrapper-164"><img
+                        src="https://www.gravatar.com/avatar/90e6eb5665442d70692337c9cab713ea?s=328&amp;d=identicon&amp;r=PG&amp;f=1"
+                        alt="user avatar" width="164" height="164" className="bar-sm main-image"/></div>
+                    <button className="s-btn__sm s-btn__primary" type="button" onClick={handleClickOpen}>Change picture</button>
+
+                </div>
+                <br/>
+                <br/>
+                <h4>Display Name</h4>
+                <div className="d-flex ps-relative">
+                    <input className="s-input wmx4 sm:wmx-initial" id="displayName" name="DisplayName" type="text"
+                           value="Sam" data-default="Sakshi Mishra" maxLength="30" tabIndex="1" data-site="Sam"/>
+                </div>
+                <h4>Location</h4>
+                <div className="d-flex ps-relative">
+                    <input className="s-input wmx4 sm:wmx-initial" id="displayName" name="DisplayName" type="text"
+                           value="Sam" data-default="Sakshi Mishra" maxLength="30" tabIndex="1" data-site="Sam"/>
+                </div>
+                <h4>Title</h4>
+                <div className="d-flex ps-relative">
+                    <input className="s-input wmx4 sm:wmx-initial" id="displayName" name="DisplayName" type="text"
+                           value="Sam" data-default="Sakshi Mishra" maxLength="30" tabIndex="1" data-site="Sam"/>
+                </div>
+                <br/>
+                <button className="s-btn s-btn__primary" type="button">Save Changes</button>
             </div>
-            <MuiThemeProvider theme={theme}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} container spacing={2}>
-                        <Grid item sm={6} md={4} align="right" onClick ={handleClickOpen}>
-                            <Paper
-                                style={{ border: "2px solid", height: "200px", width: "200px" }}
-                            >
-                                <CardMedia
-                                    component="img"
-                                    height="194"
-                                    object-fit ="contain"
-                                    image={url}
-                                    alt="Profile Pic"
-                                />
-                            </Paper>
-                        </Grid>
-                        <Grid item sm={6} md={8} alignt="left" container>
-                            <Grid item xs={12} container alignItems="flex-end">
-                                <Typography variant="h4">{`${formInput.name}`}</Typography>
-                                <IconButton
-                                    style={{ backgroundColor: "orange", marginLeft: "1rem" }}
-                                    onClick={handleEdit}
-                                >
-                                    <EditIcon style={{ color: "white" }} />
-                                </IconButton>
-                            </Grid>
-                        </Grid>
-                    </Grid>
 
 
-                </Grid>
-            </MuiThemeProvider>
-        </div>
+        </Card>
     );
 }
