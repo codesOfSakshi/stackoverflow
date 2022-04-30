bc4116f0c882ff5502ef717c2d833624337aa8fc
Snichat97 <34672064+Snichat97@users.noreply.github.com>
2022-04-30 09:05:03 -0700

    Backend API and Model fixing for Questions.

--- backend/models/tag.js
+++ backend/models/tag.js
@@ -1,31 +1,25 @@
 const mongoose = require("mongoose");
 
 const tagSchema = new mongoose.Schema({
-    _id:{
-        type: String,
-        required: true,
-    },
+    // _id: {
+    //   type: mongoose.ObjectId,
+    //   required: true,
+    // },
     createdAt:{
         type:String,
-        required: true,
     },
     updatedAt:{
         type:String,
-        required: true,
     },
     description:{
         type:String,
-        required: true,
     },
     name:{
         type:String,
-        required: true,
     },
     numQuestions:{
         type:String,
-        required: true,
     }
 });
 
-const tagModel = mongoose.model('tag', tagSchema);
-module.exports = tagModel;
+module.exports = mongoose.model('tag', tagSchema);
