9854c287c61ad6c186f6df6da9ae1ab53035a91a
sujatha.nadimpalli@sjsu.edu <sujatha.nadimpalli@sjsu.edu>
2022-05-03 16:56:34 -0700

    Removed Editor from QuestionDisplay

--- backend/server.js
+++ backend/server.js
@@ -106,8 +106,4 @@ app.use("/api/user", user);
 app.use("/api/admin", admin);
 app.use('/api/questions',question);
 app.use('/api/tags', tagRoute);
-<<<<<<< HEAD
 app.use('/api/messages', messageRoute);
-=======
-app.use('/api/messages', messageRoute);
->>>>>>> master
