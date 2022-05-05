84deb864f76ec5813b0ebbae6eab01bc6428b9ec
Snichat97 <34672064+Snichat97@users.noreply.github.com>
2022-05-05 11:29:25 -0700

    Update QuestionDisplay.

--- frontend/src/components/questions/QuestionDisplay.jsx
+++ frontend/src/components/questions/QuestionDisplay.jsx
@@ -55,8 +55,7 @@ function QuestionsPage() {
         <div>
             <div style={{ width: '60rem',textAlign:'justify'}}>
 
-                <Row style={{width:"200px", marginTop:"30px", marginLeft: "0.5px"}}>
-                    <svg data-icon="svg-icon-bg Bookmark" class="native"></svg>
+                <Row style={{width:"200px", marginTop:"30px", marginLeft: "0.5px",float:"right"}}>
                     <Button onClick={addBookmark} style={{float:"right"}}>Bookmark</Button>
                 </Row>
                 <h2>
@@ -92,18 +91,22 @@ function QuestionsPage() {
                     </p>
                 </Row>
 
-                <Row style={{width:"200px", marginTop:"30px", marginLeft: "0.5px"}}>
+                {/* <Row style={{width:"200px", marginTop:"30px", marginLeft: "0.5px"}}>
+                    <div style={{float:"right"}} >
                     <Button onClick={navigateToEdit} style={{float:"right"}}>Edit Question</Button>
-                </Row>
+                    </div>
+                </Row> */}
            
-            <div class="displayFlex" style={{"margin-bottom":"1rem"}}>
+            <div class="displayFlex" style={{"margin-bottom":"3rem"}}>
                 {question.tags && question.tags.map( tag =>{
-                    return(<>
-                    <Badge 
-                    style={{"marginLeft":"3px"}}>
+                    return(
+                    <div class="s-badge s-badge__moderator" style={{margin:"0.5rem"}}>
                         {tag}
-                    </Badge>
-                    </>)})} 
+                    </div>
+                    )})} 
+                    <Col style={{float:"right"}} >
+                    <Button onClick={navigateToEdit} style={{float:"right",margin:"0.5rem"}}>Edit Question</Button>
+                    </Col>
             </div>
             <hr></hr>
             <div style={{backgroundColor : "#f5f6f6", display: "flex", fontFamily: "sans-serif", justifyContent: "center", alignItems: "center", height: "10vh", border: "none", outline: "none"}}>
@@ -139,7 +142,7 @@ function QuestionsPage() {
                             {ans.upVotes.length} votes */}
                                     <Upvote idx={idx}  downVote={onDownVoteClick} upVote={onUpVoteClick} object={ans} type="answer" />
                                 </Col>
-                                <Col style={{ marginLeft: "64px", marginTop: "-30px" }}>
+                                <Col style={{ marginLeft: "64px", marginTop: "-115px" }}>
                                     {ans.description}
                                     <div style={{ backgroundColor: "#f5f6f6", display: "flex", fontFamily: "sans-serif", justifyContent: "center", alignItems: "center", height: "10vh", border: "none", outline: "none" }}>
                                         <form style={{ height: "20px", width: "100%", border: "none", backgroundColor: "transparent", borderBottom: "2px solid #aaa", resize: "none", outline: "none" }}>
