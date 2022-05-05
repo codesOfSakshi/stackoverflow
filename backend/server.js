a36e4b883639a9ff756da8763b33dece686643dd
Snichat97 <34672064+Snichat97@users.noreply.github.com>
2022-05-05 15:06:53 -0700

    Backend Mongo Server pointing change.

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.listen(PORT, () => {
 
 // const mongoURI =
 //   "mongodb+srv://user1:user1@cluster0.olc4f.mongodb.net/stackover?retryWrites=true&w=majority";
-const mongoURI = `mongodb://127.0.0.1:27017/stackoverflow`;
+const mongoURI = `mongodb+srv://SnigdhaAWSMongo:AWSPa$$wordMongo@cluster0.fj6vo.mongodb.net/Stackoverflow?retryWrites=true&w=majority`;
 let options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
