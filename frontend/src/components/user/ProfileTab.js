3a0864b7bad046b850fd281f4ca89860a8d83853
sakshi.mishra@sjsu.edu <sakshi.mishra@sjsu.edu>
2022-05-05 23:46:01 -0700

    userId in component

--- frontend/src/components/user/ProfileTab.js
+++ frontend/src/components/user/ProfileTab.js
@@ -54,7 +54,7 @@ const ProfileTab = () => {
         <UserStats reach={reach} reputation={reputation} answerCount={answerCount} questionCount={questionCount}/>
         <UserBadgeInfo/>
         <UserTagList/>
-        <UserTopPostList/>
+        <UserTopPostList userId={userId}/>
       </div>
   )
 }
