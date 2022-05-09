390b33e880de18987b2bcae631f51994b228ca8d
Snichat97 <34672064+Snichat97@users.noreply.github.com>
2022-05-09 13:36:05 -0700

    Update Ask question Editor.

--- frontend/src/components/questions/AskQuestionEditor.jsx
+++ frontend/src/components/questions/AskQuestionEditor.jsx
@@ -23,6 +23,7 @@ function AskQuestionEditor(props) {
   const[descripiton,setDescription]=useState("")
   const [imageArray, setImage] = useState([]);
   const [uploadUrl,setUploadUrl] = useState([]);
+  const [error,setError] = useState("");
 
   const cookies = new Cookies();
     const token = localStorage.getItem("token");
@@ -61,6 +62,10 @@ function AskQuestionEditor(props) {
   const submitHandler =(e)=>{
     console.log(e);
     e.preventDefault();
+    if(!descripiton){
+      setError("Please Enter a valid Description")
+    }
+    else{
     var payload ={
       user: decoded._id,
       title: e.target.formBasicTitle.value,
@@ -74,6 +79,7 @@ function AskQuestionEditor(props) {
       var path = "/question/"+response.data.message
       navigate(path)
       })
+    }
   }
 
   const deleteTag = (e,val) =>{
@@ -227,6 +233,9 @@ function AskQuestionEditor(props) {
           {return(<a class="flex--item s-tag s-tag__moderator" href="#">{tag}
           <span class="s-tag--dismiss"> <div val={tag} onClick={(e)=>deleteTag(e,tag)}>X</div></span></a>)})}
       </div>
+      </div>
+      <div>
+        {error}
       </div>
 
         <center>
